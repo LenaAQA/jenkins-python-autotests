b966e4b795a622f81abe7309d65fe6cf019ccd93
lena <alenaxxxx94@gmail.com>
2025-08-18 16:33:10 +0300

    delete tests

--- tests/multi_configuration_project_configuration/test_edit_description.py
+++
@@ -1,8 +0,0 @@
-from tests.multi_configuration_project_configuration.data import updated_description_text
-
-
-def test_edit_description_from_project_page(multi_config_project_with_description):
-    multi_config_project_with_description.edit_description(updated_description_text)
-    text_actual = multi_config_project_with_description.get_saved_description_text()
-    assert text_actual == updated_description_text, f"Expected '{updated_description_text}', but got '{text_actual}'"
-
--- tests/new_folder_configuration/folder_data.py
+++
@@ -1,7 +0,0 @@
-
-class FolderNames:
-    item_name = "Folder-one"
-    item_display_name = "My first folder"
-    item_description = "This is a sanity test description"
-
-
